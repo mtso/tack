3613470ba5f81a3ab281ae15b4ea26c43cdf208a
mtso <mtso@tuta.io>
2017-05-10 12:19:27 -0700

    Make command map
    Refactor command handler map

--- cmd/tackdb/main.go
+++ cmd/tackdb/main.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strings"
+
 	"github.com/mtso/tack"
 )
 
 func main() {
+	handle := tack.MakeHandler()
+	fmt.Println(handle)
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
-		tack.Hello(string(scanner.Text()))
+		str := strings.Split(scanner.Text(), " ")
+
+		if cmd, ok := handle[strings.ToUpper(str[0])]; !ok {
+			fmt.Println("UNRECOGNIZED COMMAND")
+		} else {
+			args := convertArgs(str[1:])
+
+			if resp := cmd(args...); resp != nil {
+				fmt.Println(resp)
+			}
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		tack.Hello(err.Error())
+
 	}
+}
+
+func convertArgs(input []string) (args []interface{}) {
+	args = make([]interface{}, len(input))
+	for i := range args {
+		args[i] = interface{}(input[i])
+	}
+	return
 }
 
 // import (
--- db.go
+++ db.go
@@ -2,12 +2,45 @@ package tack
 
 import "fmt"
 
-type Db struct {
-	store map[string]interface{}
-	count map[interface{}]int
+type db struct {
+	store  map[interface{}]interface{}
+	count  map[interface{}]int
+	isUndo bool
 }
 
-func (db *Db) Set(name string, value interface{}) {
-	db.store[name] = value
-	db.count[value] += 1
+type Command func(args ...interface{}) interface{}
+
+func CreateDb() *db {
+	return &db{
+		store: make(map[interface{}]interface{}),
+		count: make(map[interface{}]int),
+	}
+}
+
+func MakeHandler() map[string]Command {
+	db := CreateDb()
+	return map[string]Command{
+		"GET":        db.Get,
+		"SET":        db.Set,
+		"NUMEQUALTO": db.NumEqualTo,
+	}
+}
+
+func (db *db) Get(args ...interface{}) (value interface{}) {
+	value, ok := db.store[args[0]]
+	if !ok {
+		return "NULL"
+	}
+	return
+}
+
+func (db *db) NumEqualTo(args ...interface{}) (count interface{}) {
+	count, _ = db.count[args[0]]
+	return
+}
+
+func (db *db) Set(args ...interface{}) interface{} {
+	db.store[args[0]] = args[1]
+	db.count[args[1]] += 1
+	return nil
 }
